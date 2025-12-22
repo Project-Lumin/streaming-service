95eac77b997eab982d396735ff4ec8779e2d1d7b
Axalate <ridmatharaka99@gmail.com>
2025-12-22 23:32:48 +0530

    configs added

---
+++ api/upload_video_handler.go
@@ -0,0 +1,8 @@
+package api
+
+import "github.com/gofiber/fiber/v2"
+
+func UploadVideoHandler(c *fiber.Ctx)error {
+
+	return c.JSON(nil)
+}
--- router/router.go
+++ router/router.go
@@ -2,11 +2,21 @@ package router
 
 import (
 	"streaming-service/api"
+	"streaming-service/config"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Route(g *fiber.App) {
 	v1 := g.Group("/v1")
+
+
 	v1.Get("/fetch/segment", api.FetchSegmentHandler)
+	v1.Post("/upload" , api.UploadVideoHandler)
+
+
+
+	g.Get("/health-check" , func(c *fiber.Ctx) error {
+		return c.JSON(map[string]interface{}{"status" : "server is up and running" , "version" : config.ReleaseVersion})
+	})
 }
