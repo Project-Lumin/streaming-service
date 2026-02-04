8498a94955c214f40c6d0847cf3d13ffce2d564e
Axalate <ridmatharaka99@gmail.com>
2026-02-04 16:47:06 +0530

    video handlers

--- api/upload_video_handler.go
+++ api/upload_video_handler.go
@@ -40,24 +40,26 @@ func UploadVideoHandler(c *fiber.Ctx) error {
 	if err := os.MkdirAll(dashFolder, 0755); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(utils.ErrorResponse(err.Error()))
 	}
+	go func(savePath string) {
+		dashCmd := exec.Command("ffmpeg", "-i", savePath,
+			"-c:v", "libx264", "-pix_fmt", "yuv420p",
+			"-c:a", "aac",
+			"-f", "hls", // <--- Change dash to hls
+			"-hls_time", "5", // Segment duration
+			"-hls_playlist_type", "vod",
+			"-hls_segment_filename", dashFolder+"/segment_%03d.ts",
+			dashFolder+"/master.m3u8", // <--- Output is .m3u8
+		)
+		if err := dashCmd.Run(); err != nil {
+			log.Println("DASH transcoding failed:", err, "[file_id] - ", fileId)
+		} else {
+			log.Println("DASH transcoding completed", "[file_id] - ", fileId)
+		}
+	}(filePath)
 
-	dashCmd := exec.Command("ffmpeg", "-i", filePath,
-		"-c:v", "libx264", "-c:a", "aac",
-		"-f", "dash",
-		"-seg_duration", "5",
-		"-use_template", "1",
-		"-use_timeline", "1",
-		dashFolder+"/manifest.mpd",
-	)
-	if err := dashCmd.Run(); err != nil {
-		log.Println("DASH transcoding failed:", err, "[file_id] - ", fileId)
-	} else {
-		log.Println("DASH transcoding completed", "[file_id] - ", fileId)
-	}
-
-	err = os.Remove(filePath)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(utils.ErrorResponse(err.Error()))
-	}
+	// err = os.Remove(filePath)
+	// if err != nil {
+	// 	return c.Status(fiber.StatusInternalServerError).JSON(utils.ErrorResponse(err.Error()))
+	// }
 	return c.Status(fiber.StatusCreated).JSON(utils.SuccessResponse(fileId))
 }
--- router/router.go
+++ router/router.go
@@ -10,7 +10,7 @@ import (
 func Route(g *fiber.App) {
 	v1 := g.Group("/v1")
 
-	v1.Get("/fetch/segment", api.FetchSegmentHandler)
+	v1.Get("/fetch/:videoId/*", api.FetchSegmentHandler)
 	v1.Post("/upload", api.UploadVideoHandler)
 	v1.Post("/video", api.CreateVideoDataHandler)
 	v1.Get("/videos", api.FindallVideosHandler)
